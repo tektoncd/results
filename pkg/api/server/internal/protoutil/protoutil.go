42ca2dc8b390e9a863c5f8c330e18663f789081f
Billy Lynch <wlynch@google.com>
2020-12-22 12:26:30 -0500

    protoutil: Have Any helpers fail tests instead of panic.
    This modifies the behavior of the Any proto helpers to take in a testing
    struct to cause tests to fail gracefully instead of a hard-panic.

--- pkg/api/server/internal/protoutil/protoutil.go
+++ pkg/api/server/internal/protoutil/protoutil.go
@@ -2,26 +2,29 @@
 package protoutil
 
 import (
+	"testing"
+
 	"google.golang.org/protobuf/proto"
 	"google.golang.org/protobuf/types/known/anypb"
 )
 
-// Any wraps a proto message in an Any proto. If there is any problem,
-// this panics.
-func Any(m proto.Message) *anypb.Any {
+// Any wraps a proto message in an Any proto, or causes the test to fail.
+func Any(t testing.TB, m proto.Message) *anypb.Any {
+	t.Helper()
 	a, err := anypb.New(m)
 	if err != nil {
-		panic(err)
+		t.Fatalf("error wrapping Any proto: %v", err)
 	}
 	return a
 }
 
 // AnyBytes returns the marshalled bytes of an Any proto wrapping the given
-// message. If there is any problem, this panics.
-func AnyBytes(m proto.Message) []byte {
-	b, err := proto.Marshal(Any(m))
+// message, or causes the test to fail.
+func AnyBytes(t testing.TB, m proto.Message) []byte {
+	t.Helper()
+	b, err := proto.Marshal(Any(t, m))
 	if err != nil {
-		panic(err)
+		t.Fatalf("error marshalling Any proto: %v", err)
 	}
 	return b
 }
