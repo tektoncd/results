02f7f488d708ec6630916433dc6927b52201c9ba
Billy Lynch <wlynch@google.com>
2020-12-23 14:33:15 -0500

    Add missing license headers.
    Doesn't add a test to verify headers, since addlicense doesn't exist in
    the testrunner image. :(

--- pkg/api/server/db/errors/sqlite/sqlite.go
+++ pkg/api/server/db/errors/sqlite/sqlite.go
@@ -1,3 +1,17 @@
+// Copyright 2020 The Tekton Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 // Package sqlite provides sqlite-specific error checking. This is
 // purposefully broken out from the rest of the errors package so that we can
 // isolate go-sqlite3's cgo dependency away from the main MySQL based library
