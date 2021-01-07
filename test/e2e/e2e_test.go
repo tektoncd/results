064656fca10581c76f7b9fa03d510902f288b3e0
Billy Lynch <wlynch@google.com>
2021-01-07 18:05:07 -0500

    e2e: Add TaskRun for running kind tests.
    This adds a TaskRun for executing kind tests. To start, this is a single
    TaskRun, but I plan to break this out into its own reusable Task for the
    catalog in the future.
    
    The configs here are heavily based on the configs in
    https://github.com/kind-ci/examples and
    https://github.com/kubernetes-sigs/kind/issues/303.
    
    This was done in favor of the Prow based config, since the cluster
    failed to come up, dispite the various labels set (likely the settings
    weren't applying / we are missing some other config in our prow
    cluster). Since we want to move towards Tekton based CI in the future
    anyway, it was simpler to handle container setup in a Task from scratch.

--- test/e2e/e2e_test.go
+++ test/e2e/e2e_test.go
@@ -1,3 +1,17 @@
+// Copyright 2021 The Tekton Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 // +build e2e
 
 package e2e
@@ -21,7 +35,7 @@ const (
 
 func TestTaskRun(t *testing.T) {
 	tr := new(v1beta1.TaskRun)
-	b, err := ioutil.ReadFile("taskrun.yaml")
+	b, err := ioutil.ReadFile("testdata/taskrun.yaml")
 	if err != nil {
 		t.Fatalf("ioutil.Readfile: %v", err)
 	}
@@ -57,7 +71,7 @@ func TestTaskRun(t *testing.T) {
 
 func TestPipelineRun(t *testing.T) {
 	pr := new(v1beta1.PipelineRun)
-	b, err := ioutil.ReadFile("pipelinerun.yaml")
+	b, err := ioutil.ReadFile("testdata/pipelinerun.yaml")
 	if err != nil {
 		t.Fatalf("ioutil.Readfile: %v", err)
 	}
