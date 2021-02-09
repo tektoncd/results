c669f6a3a3279d2a14b79ae533ed31d701a92dab
Billy Lynch <wlynch@google.com>
2021-02-09 12:26:27 -0500

    simpleui: Add query, parent fields.
    Adds fields for linking to Record parents and query field for filtering
    results.

--- tools/simpleui/template.go
+++ tools/simpleui/template.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"strings"
 
 	"google.golang.org/protobuf/encoding/prototext"
 	"google.golang.org/protobuf/types/known/anypb"
@@ -16,4 +17,9 @@ func textproto(a *anypb.Any) (string, error) {
 		return "", err
 	}
 	return prototext.Format(m), nil
+}
+
+func parent(in string) string {
+	s := strings.Split(in, "/")
+	return strings.Join(s[:3], "/")
 }
