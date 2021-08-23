66e7763a7a7e32b05ebefa69ea4dd93fc900f384
Billy Lynch <wlynch@google.com>
2021-08-23 15:34:23 -0400

    release.sh: Use kustomize to generate release yaml.
    This uses envsubst + kustomize to apply any overlays to resources as part of the
    release process.
    
    envsubst is used to evaluate environment variables in place before
    handing off to kustomize, since kustomize does not support reading from
    stdin (https://github.com/kubernetes-sigs/kustomize/issues/2985).

--- release/release.sh
+++ release/release.sh
@@ -1,11 +1,16 @@
 #! /bin/bash
 
-set -e
+set -ex
 
 ROOT="$(git rev-parse --show-toplevel)"
 # Default to short SHA if release version not set.
-RELEASE_VERSION=${RELEASE_VERSION:-"$(git rev-parse --short HEAD)"}
+export RELEASE_VERSION=${RELEASE_VERSION:-"$(git rev-parse --short HEAD)"}
 
 export KO_DOCKER_REPO=${KO_DOCKER_REPO:-"ko.local"}
 
-ko resolve -P -f ${ROOT}/config -t ${RELEASE_VERSION} | sed "s/devel/${RELEASE_VERSION}/g" > ${ROOT}/release/release.yaml
+RELEASE_DIR="${ROOT}/release"
+# Apply templated values from environment.
+envsubst < ${RELEASE_DIR}/kustomization.yaml | tee ${RELEASE_DIR}/kustomization.yaml  
+
+# Apply kustomiation + build images + generate yaml
+kubectl kustomize ${RELEASE_DIR} | ko resolve -P -f - -t ${RELEASE_VERSION} > ${RELEASE_DIR}/release.yaml
