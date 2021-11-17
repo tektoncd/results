7d716331d8116469a5f3084dbec62e2b1a3ffe01
Billy Lynch <wlynch@google.com>
2021-11-17 14:09:08 -0500

    tkn-result: Output list as table by default.
    Implements a tabwriter to display denser data formats for users, similar
    to kubectl.
    
    Old formats are still accessible by specifying the -o flag.

--- tools/tkn-results/cmd/root_test.go
+++ tools/tkn-results/cmd/root_test.go
@@ -1,9 +1,12 @@
 package cmd
 
 import (
+	"bufio"
 	"bytes"
 	"fmt"
+	"strings"
 	"testing"
+	"time"
 
 	"github.com/google/go-cmp/cmp"
 	pb "github.com/tektoncd/results/proto/v1alpha2/results_go_proto"
@@ -12,6 +15,8 @@ import (
 	"google.golang.org/protobuf/proto"
 	"google.golang.org/protobuf/reflect/protoreflect"
 	"google.golang.org/protobuf/testing/protocmp"
+	"google.golang.org/protobuf/types/known/anypb"
+	"google.golang.org/protobuf/types/known/timestamppb"
 )
 
 func TestPrintProto(t *testing.T) {
@@ -42,4 +47,73 @@ func testPrintProto(t *testing.T, kind string, unmarshal func([]byte, protorefle
 			t.Fatal(diff)
 		}
 	})
+}
+
+func TestPrintProto_Tab(t *testing.T) {
+	ts := time.Now().Local().Truncate(time.Second)
+
+	for _, tc := range []struct {
+		m    proto.Message
+		want [][]string
+	}{
+		{
+			m: &pb.ListResultsResponse{
+				Results: []*pb.Result{{
+					Name:        "a",
+					CreatedTime: timestamppb.New(ts),
+					UpdatedTime: timestamppb.New(ts),
+				}},
+			},
+			want: [][]string{
+				{"Name", "Start", "Update"},
+				{"a", ts.String(), ts.String()},
+			},
+		},
+		{
+			m: &pb.ListRecordsResponse{
+				Records: []*pb.Record{{
+					Name:        "a",
+					CreatedTime: timestamppb.New(ts),
+					UpdatedTime: timestamppb.New(ts),
+					Data: &anypb.Any{
+						TypeUrl: "tacocat",
+					},
+				}},
+			},
+			want: [][]string{
+				{"Name", "Type", "Start", "Update"},
+				{"a", "tacocat", ts.String(), ts.String()},
+			},
+		},
+	} {
+		t.Run(fmt.Sprintf("%T", tc.m), func(t *testing.T) {
+			// Dump output to buffer
+			b := new(bytes.Buffer)
+			if err := printproto(b, tc.m, "tab"); err != nil {
+				t.Fatal(err)
+			}
+
+			// Normalize output by tokenizing each line to remove soft-tabs.
+			var got [][]string
+			scanner := bufio.NewScanner(b)
+			for scanner.Scan() {
+				line := []string{}
+				// Tabwriter is configured for at least 2 spaces between each field.
+				for _, s := range strings.Split(scanner.Text(), "  ") {
+					// There might be multiple "tabs" for each field that
+					// causes split to contain ""s - only look at non-empty
+					// tokens.
+					if s != "" {
+						line = append(line, strings.TrimSpace(s))
+					}
+				}
+				fmt.Println(line)
+				got = append(got, line)
+			}
+
+			if diff := cmp.Diff(tc.want, got); diff != "" {
+				t.Error(diff)
+			}
+		})
+	}
 }
