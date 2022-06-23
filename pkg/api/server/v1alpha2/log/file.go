7e99e5d75e589cd8096cec6952f14042a2ddfa5e
Adam Kaplan <adam.kaplan@redhat.com>
2022-06-23 12:13:54 -0400

    FileLogStreamer: Initial Implementation
    Simple implementation to stream a log file from disk to an io.Writer.

--- pkg/api/server/v1alpha2/log/file.go
+++ pkg/api/server/v1alpha2/log/file.go
@@ -1,14 +1,17 @@
 package log
 
 import (
+	"bufio"
 	"fmt"
 	"io"
+	"os"
 )
 
 type fileLogStreamer struct {
 	path string
 }
 
+// NewFileLogStreamer returns a LogStreamer that streams directly from a log file on local disk.
 func NewFileLogStreamer(trl *TaskRunLog) (LogStreamer, error) {
 	if trl.File == nil {
 		return nil, fmt.Errorf("file to stream is not specified")
@@ -22,6 +25,23 @@ func (*fileLogStreamer) Type() string {
 	return string(FileLogType)
 }
 
-func (f *fileLogStreamer) WriteTo(w io.Writer) (int64, error) {
-	return 0, nil
+func (f *fileLogStreamer) WriteTo(w io.Writer) (n int64, err error) {
+	_, err = os.Stat(f.path)
+	if err != nil {
+		return 0, fmt.Errorf("failed to stat %s: %v", f.path, err)
+	}
+	file, err := os.Open(f.path)
+	if err != nil {
+		return 0, fmt.Errorf("failed to open file %s: %v", f.path, err)
+	}
+	defer func() {
+		closeErr := file.Close()
+		if err == nil && closeErr != nil {
+			err = closeErr
+		}
+	}()
+	// Use the bufferred reader to ensure file contents are not read entirely into memory
+	reader := bufio.NewReader(file)
+	n, err = reader.WriteTo(w)
+	return
 }
---
+++ pkg/api/server/v1alpha2/log/file_test.go
@@ -0,0 +1,54 @@
+package log
+
+import (
+	"bytes"
+	"io/ioutil"
+	"os"
+	"testing"
+)
+
+func TestFileLogStreamer(t *testing.T) {
+	dir, err := ioutil.TempDir("", "test-filelogstreamer-*")
+	if err != nil {
+		t.Fatalf("failed to create test directory: %v", err)
+	}
+	tmp, err := ioutil.TempFile(dir, "temp-file-*.log")
+	if err != nil {
+		t.Fatalf("failed to create test log file: %v", err)
+	}
+	t.Logf("test log file: %s", tmp.Name())
+	t.Cleanup(func() {
+		err := tmp.Close()
+		if err != nil {
+			t.Fatalf("failed to close test log file: %v", err)
+		}
+		err = os.RemoveAll(dir)
+		if err != nil {
+			t.Fatalf("failed to remove directory %s: %v", dir, err)
+		}
+	})
+	expected := "Hello World!"
+	_, err = tmp.WriteString("Hello World!")
+	if err != nil {
+		t.Fatalf("failed to write test message: %v", err)
+	}
+	trl := &TaskRunLog{
+		Type: FileLogType,
+		File: &FileLogTypeSpec{
+			Path: tmp.Name(),
+		},
+	}
+	streamer, err := NewFileLogStreamer(trl)
+	if err != nil {
+		t.Fatalf("failed to create file log streamer: %v", err)
+	}
+	outBuf := &bytes.Buffer{}
+	_, err = streamer.WriteTo(outBuf)
+	if err != nil {
+		t.Fatalf("WriteTo: failed with error: %v", err)
+	}
+	actual := outBuf.String()
+	if expected != actual {
+		t.Errorf("WriteTo: expected %q, got %q", expected, actual)
+	}
+}
