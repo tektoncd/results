a1d03963b752507e4aafb386462a8926a494ea50
Alan Ghelardi <alan.ghelardi@nubank.com.br>
2022-09-23 17:03:24 -0300

    Enable high availability through Knative leader election support.

---
+++ pkg/watcher/reconciler/leaderelection/leader_election.go
@@ -0,0 +1,48 @@
+// Copyright 2022 The Tekton Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// package leaderelection provides a few utilities to help us to enable leader
+// election support in the Watcher controllers.
+package leaderelection
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/labels"
+	"k8s.io/apimachinery/pkg/types"
+	"knative.dev/pkg/reconciler"
+)
+
+// Lister is a generic signature of Lister.List functions, by allowing us to
+// support various listers in the NewLeaderAwareFuncs function below.
+type Lister[O metav1.Object] func(labels.Selector) ([]O, error)
+
+// NewLeaderAwareFuncs returns a new reconciler.LeaderAwareFuncs object to be
+// used in our controllers.
+func NewLeaderAwareFuncs[O metav1.Object](lister Lister[O]) reconciler.LeaderAwareFuncs {
+	return reconciler.LeaderAwareFuncs{
+		PromoteFunc: func(bucket reconciler.Bucket, enqueue func(reconciler.Bucket, types.NamespacedName)) error {
+			objects, err := lister(labels.Everything())
+			if err != nil {
+				return err
+			}
+			for _, object := range objects {
+				enqueue(bucket, types.NamespacedName{
+					Namespace: object.GetNamespace(),
+					Name:      object.GetName(),
+				})
+			}
+			return nil
+		},
+	}
+}
--- pkg/watcher/reconciler/taskrun/reconciler.go
+++ pkg/watcher/reconciler/taskrun/reconciler.go
@@ -4,23 +4,33 @@ import (
 	"context"
 	"time"
 
+	"knative.dev/pkg/controller"
+	knativereconciler "knative.dev/pkg/reconciler"
+
 	"github.com/tektoncd/pipeline/pkg/client/clientset/versioned"
 	"github.com/tektoncd/pipeline/pkg/client/listers/pipeline/v1beta1"
 	"github.com/tektoncd/results/pkg/watcher/reconciler"
 	"github.com/tektoncd/results/pkg/watcher/reconciler/dynamic"
 	pb "github.com/tektoncd/results/proto/v1alpha2/results_go_proto"
 	"go.uber.org/zap"
+	"k8s.io/apimachinery/pkg/types"
 	"k8s.io/client-go/tools/cache"
 	"knative.dev/pkg/logging"
 )
 
 type Reconciler struct {
+	// Inline LeaderAwareFuncs to support leader election.
+	knativereconciler.LeaderAwareFuncs
+
 	client    pb.ResultsClient
 	lister    v1beta1.TaskRunLister
 	k8sclient versioned.Interface
 	cfg       *reconciler.Config
 	enqueue   func(interface{}, time.Duration)
 }
+
+// Check that our Reconciler is LeaderAware.
+var _ knativereconciler.LeaderAware = (*Reconciler)(nil)
 
 func (r *Reconciler) Reconcile(ctx context.Context, key string) error {
 	log := logging.FromContext(ctx)
@@ -31,7 +41,13 @@ func (r *Reconciler) Reconcile(ctx context.Context, key string) error {
 		log.Errorf("invalid resource key: %s", key)
 		return nil
 	}
-	pr, err := r.lister.TaskRuns(namespace).Get(name)
+
+	if !r.IsLeaderFor(types.NamespacedName{Namespace: namespace, Name: name}) {
+		log.Info("Skipping TaskRun key because this instance isn't its leader")
+		return controller.NewSkipKey(key)
+	}
+
+	tr, err := r.lister.TaskRuns(namespace).Get(name)
 	if err != nil {
 		return err
 	}
@@ -41,7 +57,7 @@ func (r *Reconciler) Reconcile(ctx context.Context, key string) error {
 	}
 
 	dyn := dynamic.NewDynamicReconciler(r.client, k8sclient, r.cfg, r.enqueue)
-	if err := dyn.Reconcile(ctx, pr); err != nil {
+	if err := dyn.Reconcile(ctx, tr); err != nil {
 		return err
 	}
 
