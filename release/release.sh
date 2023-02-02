e3511eba3dccc0a214dbbc044051dcfeab6920ed
Adam Kaplan <adam.kaplan@redhat.com>
2023-02-01 19:34:03 -0500

    Organize Results Kustomization with Components
    Utilize Kustomize Components [1] to create variants of Results. The Logs
    API introduces a potential explosion in configuration options. Releasing
    with Logs may require multiple changes to the deployment - some
    potentially incompatible. Components provides us a mechanism to manage
    the exploding test matrix. The config directory has been reorganized as
    follows:
    
    - base
    |---- <base configuration manifests>
    - components
    |---- local-db (deploys PostgresDB)
    |---- metadata (common metadata and transformations)
    - overlays
    |----- default-local-db (local PostgresDB, logs disabled)
    
    Tests and releases are now based off of `overlays/default-local-db`.
    Future enhancements can add additional overlays for testing or community
    examples.
    
    [1] https://github.com/kubernetes-sigs/kustomize/blob/master/examples/components.md

--- release/release.sh
+++ release/release.sh
@@ -11,7 +11,7 @@ export KO_DOCKER_REPO=${KO_DOCKER_REPO:-"ko.local"}
 RELEASE_DIR="${ROOT}/release"
 # Apply templated values from environment.
 sed -i "s/devel$/${RELEASE_VERSION}/g" ${RELEASE_DIR}/kustomization.yaml
-sed -i "s/devel$/${RELEASE_VERSION}/g" ${ROOT}/config/config-info.yaml  
+sed -i "s/devel$/${RELEASE_VERSION}/g" ${ROOT}/config/base/config-info.yaml  
 
 # Apply kustomiation + build images + generate yaml
 kubectl kustomize ${RELEASE_DIR} | ko resolve --platform "linux/amd64,linux/ppc64le,linux/s390x" -P -f - -t ${RELEASE_VERSION} > ${RELEASE_DIR}/release.yaml
