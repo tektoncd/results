43f400acde220ba646e5b4b20c4c1f68bdb65f89
manuelwall <8551951+manuelwallrapp@users.noreply.github.com>
2023-04-05 14:46:59 +0200

    Update cmd/watcher/main.go
    Co-authored-by: Alan Ghelardi <alan.ghelardi@gmail.com>

--- cmd/watcher/main.go
+++ cmd/watcher/main.go
@@ -63,7 +63,7 @@ var (
 	logsAPI                 = flag.Bool("logs_api", true, "Disable sending logs. If not set, the logs will be sent only if server support API for it")
 	labelSelector           = flag.String("label_selector", "", "Selector (label query) to filter objects to be deleted. Matching objects must satisfy all labels requirements to be eligible for deletion")
 	requeueInterval         = flag.Duration("requeue_interval", 10*time.Minute, "How long the Watcher waits to reprocess keys on certain events (e.g. an object doesn't match the provided selectors)")
-	namespace               = flag.String("namespace", corev1.NamespaceAll, "Should the Watcher only watch a single namespace, then this values needs to be set to the namespace name otherwise leave it empty.")
+	namespace               = flag.String("namespace", corev1.NamespaceAll, "Should the Watcher only watch a single namespace, then this value needs to be set to the namespace name otherwise leave it empty.")
 )
 
 func main() {
