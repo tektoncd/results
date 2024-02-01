b7905d167db5b66592e694d72f9db346b8ac7896
Khurram Baig <kbaig@redhat.com>
2024-02-01 15:46:49 +0530

    Add support for No DB release
    This add support for release_base.yaml which doesn't include db.

--- release/release.sh
+++ release/release.sh
@@ -11,7 +11,9 @@ export KO_DOCKER_REPO=${KO_DOCKER_REPO:-"ko.local"}
 RELEASE_DIR="${ROOT}/release"
 # Apply templated values from environment.
 sed -i "s/devel$/${RELEASE_VERSION}/g" ${RELEASE_DIR}/kustomization.yaml
-sed -i "s/devel$/${RELEASE_VERSION}/g" ${ROOT}/config/base/config-info.yaml  
+sed -i "s/devel$/${RELEASE_VERSION}/g" ${ROOT}/config/base/config-info.yaml
 
 # Apply kustomiation + build images + generate yaml
-kubectl kustomize ${RELEASE_DIR} | ko resolve --platform "linux/amd64,linux/arm,linux/arm64,linux/ppc64le,linux/s390x" -P -f - -t ${RELEASE_VERSION} > ${RELEASE_DIR}/release.yaml
+kubectl kustomize ${RELEASE_DIR} | ko resolve --platform "linux/amd64,linux/arm,linux/arm64,linux/ppc64le,linux/s390x" -P -f - -t ${RELEASE_VERSION} > ${RELEASE_DIR}/release_base.yaml
+cp ${RELEASE_DIR}/release_base.yaml ${RELEASE_DIR}/release.yaml
+cat ${ROOT}/config/components/local-db/201-sql-deployment.yaml >> ${RELEASE_DIR}/release.yaml
