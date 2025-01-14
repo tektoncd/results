7e58bbfce5cbe7117861a6a7f2303f559423b896
Andrea Frittoli <andrea.frittoli@gmail.com>
2025-01-14 09:49:07 +0000

    Support for release to ghcr.io
    Tekton projects now release to ghcr.io. This PR adapts
    the existing release pipeline to support releasing to
    ghcr.io as well.
    
    Workload identity can still be use to publish the YAMLs
    to the gcr.io bucket, but images will be published to
    the container registry through a PAT stored on the cluster.
    
    This updates related resources and docs, and introduces
    remote resolution for tasks that are already available
    elsewhere.
    
    Signed-off-by: Andrea Frittoli <andrea.frittoli@gmail.com>

--- release/release.sh
+++ release/release.sh
@@ -5,15 +5,23 @@ set -ex
 ROOT="$(git rev-parse --show-toplevel)"
 # Default to short SHA if release version not set.
 export RELEASE_VERSION=${RELEASE_VERSION:-"$(git rev-parse --short HEAD)"}
+export GITHUB_REPO=${GITHUB_REPO:-"https://github.com/tektoncd/results"}
 
 export KO_DOCKER_REPO=${KO_DOCKER_REPO:-"ko.local"}
+
+# Create a tag for ko
+git tag ${RELEASE_VERSION}
 
 RELEASE_DIR="${ROOT}/release"
 # Apply templated values from environment.
 sed -i "s/devel$/${RELEASE_VERSION}/g" ${RELEASE_DIR}/kustomization.yaml
 sed -i "s/devel$/${RELEASE_VERSION}/g" ${ROOT}/config/base/config-info.yaml
 
-# Apply kustomiation + build images + generate yaml
-kubectl kustomize ${RELEASE_DIR} | ko resolve --platform "linux/amd64,linux/arm,linux/arm64,linux/ppc64le,linux/s390x" -P -f - -t ${RELEASE_VERSION} > ${RELEASE_DIR}/release_base.yaml
+# Apply kustomization + build images + generate yaml
+kubectl kustomize ${RELEASE_DIR} | ko resolve \
+    --image-label=org.opencontainers.image.source=${GITHUB_REPO} \
+    --platform "linux/amd64,linux/arm,linux/arm64,linux/ppc64le,linux/s390x" \
+    ${KO_EXTRA_ARGS} -f - -t ${RELEASE_VERSION} > ${RELEASE_DIR}/release_base.yaml
+
 cp ${RELEASE_DIR}/release_base.yaml ${RELEASE_DIR}/release.yaml
 kubectl kustomize ${RELEASE_DIR}/localdb >> ${RELEASE_DIR}/release.yaml
