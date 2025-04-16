43030eb952a5d8cca2cb2a42f9f6da719d55273c
Khurram Baig <kbaig@redhat.com>
2025-04-16 13:26:35 +0530

    Fix GoLint issue after latest CEL Go update

--- pkg/api/server/cel/cel.go
+++ pkg/api/server/cel/cel.go
@@ -20,7 +20,7 @@ import (
 	"log"
 
 	"github.com/google/cel-go/cel"
-	"github.com/google/cel-go/checker/decls"
+	"github.com/google/cel-go/common/decls"
 	"github.com/google/cel-go/common/types"
 	"github.com/google/cel-go/common/types/ref"
 	ppb "github.com/tektoncd/results/proto/pipeline/v1/pipeline_go_proto"
@@ -34,8 +34,8 @@ import (
 func NewEnv() (*cel.Env, error) {
 	return cel.NewEnv(
 		cel.Types(&pb.Result{}, &pb.Record{}, &ppb.PipelineRun{}, &ppb.TaskRun{}),
-		cel.Declarations(decls.NewVar("result", decls.NewObjectType("tekton.results.v1alpha2.Result"))),
-		cel.Declarations(decls.NewVar("record", decls.NewObjectType("tekton.results.v1alpha2.Record"))),
+		cel.VariableDecls(decls.NewVariable("result", cel.ObjectType("tekton.results.v1alpha2.Result"))),
+		cel.VariableDecls(decls.NewVariable("record", cel.ObjectType("tekton.results.v1alpha2.Record"))),
 	)
 }
 
--- pkg/api/server/cel/env.go
+++ pkg/api/server/cel/env.go
@@ -2,9 +2,8 @@ package cel
 
 import (
 	"github.com/google/cel-go/cel"
-	"github.com/google/cel-go/checker/decls"
+	"github.com/google/cel-go/common/types"
 	resultspb "github.com/tektoncd/results/proto/v1alpha2/results_go_proto"
-	exprpb "google.golang.org/genproto/googleapis/api/expr/v1alpha1"
 	"google.golang.org/protobuf/types/known/timestamppb"
 )
 
@@ -16,10 +15,13 @@ const (
 // NewResultsEnv creates a CEL program to build SQL filters for Result objects.
 func NewResultsEnv() (*cel.Env, error) {
 	return cel.NewEnv(
-		cel.Declarations(stringConst("PIPELINE_RUN", typePipelineRun),
-			stringConst("TASK_RUN", typeTaskRun),
-		),
-		cel.Declarations(recordSummaryStatusConsts()...),
+		cel.Constant("PIPELINE_RUN", cel.StringType, types.String(typePipelineRun)),
+		cel.Constant("TASK_RUN", cel.StringType, types.String(typeTaskRun)),
+		cel.Constant("UNKNOWN", cel.IntType, types.Int(resultspb.RecordSummary_UNKNOWN)),
+		cel.Constant("SUCCESS", cel.IntType, types.Int(resultspb.RecordSummary_SUCCESS)),
+		cel.Constant("FAILURE", cel.IntType, types.Int(resultspb.RecordSummary_FAILURE)),
+		cel.Constant("TIMEOUT", cel.IntType, types.Int(resultspb.RecordSummary_TIMEOUT)),
+		cel.Constant("CANCELLED", cel.IntType, types.Int(resultspb.RecordSummary_CANCELLED)),
 		cel.Types(&resultspb.RecordSummary{},
 			&timestamppb.Timestamp{}),
 		cel.Variable("parent", cel.StringType),
@@ -37,30 +39,12 @@ func NewResultsEnv() (*cel.Env, error) {
 // NewRecordsEnv creates a CEL program to build SQL filters for Record objects.
 func NewRecordsEnv() (*cel.Env, error) {
 	return cel.NewEnv(
-		cel.Declarations(stringConst("PIPELINE_RUN", typePipelineRun),
-			stringConst("TASK_RUN", typeTaskRun),
-		),
+		cel.Constant("PIPELINE_RUN", cel.StringType, types.String(typePipelineRun)),
+		cel.Constant("TASK_RUN", cel.StringType, types.String(typeTaskRun)),
 		cel.Variable("parent", cel.StringType),
 		cel.Variable("result_name", cel.StringType),
-		cel.Declarations(decls.NewVar("name", decls.String)),
-		cel.Declarations(decls.NewVar("data_type", decls.String)),
-		cel.Declarations(decls.NewVar("data", decls.Any)),
+		cel.Variable("name", cel.StringType),
+		cel.Variable("data_type", cel.StringType),
+		cel.Variable("data", cel.AnyType),
 	)
-}
-
-// stringConst is a helper to create a CEL string constant declaration.
-func stringConst(name, value string) *exprpb.Decl {
-	return decls.NewConst(name,
-		decls.String,
-		&exprpb.Constant{ConstantKind: &exprpb.Constant_StringValue{StringValue: value}})
-}
-
-// recordSummaryStatusConsts exposes the values of the RecordSummary_Status enum
-// as named constants.
-func recordSummaryStatusConsts() []*exprpb.Decl {
-	constants := make([]*exprpb.Decl, 0, len(resultspb.RecordSummary_Status_value))
-	for name, value := range resultspb.RecordSummary_Status_value {
-		constants = append(constants, decls.NewConst(name, decls.Int, &exprpb.Constant{ConstantKind: &exprpb.Constant_Int64Value{Int64Value: int64(value)}}))
-	}
-	return constants
 }
