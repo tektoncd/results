b986a12978f15b3661eaf1d739c2c7890860817d
Emil Natan <ena@redhat.com>
2025-07-27 11:40:23 +0300

    Refactor annotations package and switch to Server-Side Apply
    Removed business logic from the annotation package. Now it only provides
    functionality to manage annotations, but no logic when to set them.
    Consolidated check (if to apply), generation and actual object update.
    Before that change, the caller had to check if the patch is needed, call
    the generation of the patch and apply the patch. Now it's a single call
    to the Patch function.
    Switched to Server-Side Apply patches from Merge patches, which provide
    a more robust way to update k8s objects.
    Switched clients.go to a separate package to avoid cyclic imports.

--- pkg/watcher/reconciler/annotation/annotation.go
+++ pkg/watcher/reconciler/annotation/annotation.go
@@ -15,42 +15,55 @@
 package annotation
 
 import (
+	"context"
 	"encoding/json"
+	"fmt"
+	"strings"
 
+	"github.com/tektoncd/results/pkg/watcher/reconciler/client"
+	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/types"
+	"knative.dev/pkg/logging"
 )
 
 const (
+	// annotationPrefix - all annotations managed by watcher should have this prefix
+	annotationPrefix = "results.tekton.dev/"
 
 	// Result identifier.
-	Result = "results.tekton.dev/result"
+	Result = annotationPrefix + "result"
 
 	// Record identifier.
-	Record = "results.tekton.dev/record"
+	Record = annotationPrefix + "record"
 
 	// Log identifier.
-	Log = "results.tekton.dev/log"
+	Log = annotationPrefix + "log"
 
 	// EventList identifier.
-	EventList = "results.tekton.dev/eventlist"
+	EventList = annotationPrefix + "eventlist"
 
 	// Stored is an annotation that signals to the controller that a given object
 	// has been stored by the Results API.
-	Stored = "results.tekton.dev/stored"
+	Stored = annotationPrefix + "stored"
 
 	// ResultAnnotations is an annotation that integrators should add to objects in order to store
 	// arbitrary keys/values into the Result.Annotations field.
-	ResultAnnotations = "results.tekton.dev/resultAnnotations"
+	ResultAnnotations = annotationPrefix + "resultAnnotations"
 
 	// RecordSummaryAnnotations is an annotation that integrators should add to objects
 	// in order to store arbitrary keys/values into the Result.Summary.Annotations field.
 	// This allows for additional information to be associated with the summary of a record.
-	RecordSummaryAnnotations = "results.tekton.dev/recordSummaryAnnotations"
+	RecordSummaryAnnotations = annotationPrefix + "recordSummaryAnnotations"
 
 	// ChildReadyForDeletion is an annotation that signals to the controller that a given child object
 	// (e.g. TaskRun owned by a PipelineRun) is done and up to date in the
 	// API server and therefore, ready to be garbage collected.
-	ChildReadyForDeletion = "results.tekton.dev/childReadyForDeletion"
+	ChildReadyForDeletion = annotationPrefix + "childReadyForDeletion"
+
+	// FieldManager identifier to be used with Server-Side Apply patches
+	fieldManager = "tekton-results-watcher"
 )
 
 // Annotation is wrapper for Kubernetes resource annotations stored in the metadata.
@@ -59,65 +72,117 @@ type Annotation struct {
 	Value string
 }
 
-type mergePatch struct {
-	Metadata metadata `json:"metadata"`
+// Server-side apply patch structure
+type applyPatch struct {
+	APIVersion string   `json:"apiVersion"`
+	Kind       string   `json:"kind"`
+	Metadata   metadata `json:"metadata"`
 }
 
 type metadata struct {
+	Name        string            `json:"name"`
+	Namespace   string            `json:"namespace"`
 	Annotations map[string]string `json:"annotations"`
 }
 
-// Patch creates a jsonpatch path used for adding result / record identifiers as
-// well as other internal annotations to an object's annotations field.
-func Patch(object metav1.Object, annotations ...Annotation) ([]byte, error) {
-	data := mergePatch{
+// Patch builds and applies a patch with the given annotations to the object using the provided object client.
+func Patch(
+	ctx context.Context,
+	object metav1.Object,
+	objectClient client.ObjectClient,
+	annotations ...Annotation,
+) error {
+
+	logger := logging.FromContext(ctx)
+
+	// Get the API version and kind from the object
+	var apiVersion, kind string
+	if runtimeObj, ok := object.(runtime.Object); ok {
+		if gvk := runtimeObj.GetObjectKind().GroupVersionKind(); !gvk.Empty() {
+			kind = gvk.Kind
+			apiVersion = gvk.GroupVersion().String()
+		}
+	}
+	// If we couldn't determine the kind or apiVersion, fail
+	if kind == "" || apiVersion == "" {
+		logger.Errorf("could not determine apiVersion and kind from object %s/%s", object.GetNamespace(), object.GetName())
+		return fmt.Errorf("could not determine apiVersion and kind from object %s/%s", object.GetNamespace(), object.GetName())
+	}
+
+	if IsPatched(object, annotations...) {
+		logger.Debugf("Skipping CRD annotation patch: annotations are already set ObjectName: %s", object.GetName())
+		return nil
+	}
+
+	data := applyPatch{
+		APIVersion: apiVersion,
+		Kind:       kind,
 		Metadata: metadata{
+			Name:        object.GetName(),
+			Namespace:   object.GetNamespace(),
 			Annotations: map[string]string{},
 		},
 	}
 
+	// Copy existing managed annotations from the object
+	// Only include annotations that we manage (results.tekton.dev/* annotations)
+	// to avoid conflicts with other controllers using server-side apply
+	currentAnnotations := object.GetAnnotations()
+	for key, value := range currentAnnotations {
+		if strings.HasPrefix(key, annotationPrefix) {
+			data.Metadata.Annotations[key] = value
+		}
+	}
+
+	// Add/overwrite with new annotations
 	for _, annotation := range annotations {
 		if len(annotation.Value) != 0 {
 			data.Metadata.Annotations[annotation.Name] = annotation.Value
 		}
 	}
-
-	if isChildAndDone(object) {
-		data.Metadata.Annotations[ChildReadyForDeletion] = "true"
-	}
-	return json.Marshal(data)
-}
-
-// isChildAndDone returns true if the object in question is a child resource
-// (i.e. has owner references) and it's done, therefore eligible to be patched
-// with the results.tekton.dev/childReadyForDeletion annotation.
-func isChildAndDone(objecct metav1.Object) bool {
-	if len(objecct.GetOwnerReferences()) == 0 {
-		return false
+	patch, err := json.Marshal(data)
+	if err != nil {
+		return err
 	}
 
-	doneObj, ok := objecct.(interface{ IsDone() bool })
-	if !ok {
-		return false
+	force := false
+	patchOptions := metav1.PatchOptions{
+		FieldManager: fieldManager,
+		Force:        &force,
 	}
-	return doneObj.IsDone()
+	err = objectClient.Patch(ctx, object.GetName(), types.ApplyPatchType, patch, patchOptions)
+	if apierrors.IsConflict(err) {
+		// Since we only update the list of annotations we manage, there shouldn't be any conflicts unless
+		// another controller/client is updating our annotations. We log the issue and force patch.
+		// TODO: We can expose the error as a metric
+		logger.Warnf("failed to patch object %s with annotations %v due to Server-Side Apply patch conflict, using force patch.", object.GetName(), data.Metadata.Annotations)
+		force = true
+		err = objectClient.Patch(ctx, object.GetName(), types.ApplyPatchType, patch, patchOptions)
+	}
+
+	// After successful patch, update in-memory object
+	if err == nil {
+		currentAnnotations := object.GetAnnotations()
+		if currentAnnotations == nil {
+			currentAnnotations = make(map[string]string)
+		}
+		for _, ann := range annotations {
+			currentAnnotations[ann.Name] = ann.Value
+		}
+		object.SetAnnotations(currentAnnotations)
+	}
+
+	return err
 }
 
 // IsPatched returns true if the object in question contains all relevant
 // annotations or false otherwise.
 func IsPatched(object metav1.Object, annotations ...Annotation) bool {
 	objAnnotations := object.GetAnnotations()
-	if isChildAndDone(object) {
-		if _, found := objAnnotations[ChildReadyForDeletion]; !found {
-			return false
-		}
-	}
-
 	for _, annotation := range annotations {
 		if objAnnotations[annotation.Name] != annotation.Value {
 			return false
 		}
 	}
-
 	return true
 }
